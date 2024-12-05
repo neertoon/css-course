9993e9f32b93f3418b3081460b43e14b8a633e92
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2024-12-05 08:07:08 +0100

    Hamburger menu

--- PROJECT/shared.js
+++ PROJECT/shared.js
@@ -1,10 +1,42 @@
 let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.modal');
-let selectPlanButton = document.querySelectorAll('.plan button');
+let selectPlanButtons = document.querySelectorAll('.plan button');
 
-for (let button of selectPlanButton) {
+var modalNoButton = document.querySelector(".modal__action--negative");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+for (let button of selectPlanButtons) {
     button.addEventListener('click', function() {
         modal.style.display = 'block';
+
+        // modal.classList.add("open");
+        // backdrop.classList.add("open");
     });
 }
+
+backdrop.addEventListener("click", function() {
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove("open");
+    closeModal();
+});
+
+if (modalNoButton) {
+    modalNoButton.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+    //   backdrop.style.display = "none";
+    //   modal.style.display = "none";
+    if (modal) {
+        modal.classList.remove("open");
+    }
+    backdrop.classList.remove("open");
+}
+
+// toggleButton.addEventListener("click", function() {
+//     mobileNav.classList.add("open");
+//     backdrop.classList.add("open");
+// });
+
 
